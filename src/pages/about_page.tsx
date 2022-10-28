65a4594227d6735bc24f462ac6668298fdbc3b04
Roman Borisov <rborisov@codemastersintl.com>
2022-10-28 18:55:07 +0400

    * added feedback

--- src/pages/about_page.tsx
+++ src/pages/about_page.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 export function AboutPage() {
     let navigate = useNavigate(); 
     return (
+        /*Wrapper2 плохое название, не нужно делать блоки индивидуальными только добавляя цифру к его названию*/
         <div className="wrapper wrapper2">
             <header >
                 <div className="row container">
